18fc18659d72049b3674c9df6ed2f46baa13c448
neeldhara <neeldhara.misra@gmail.com>
2022-08-02 23:32:26 +0530

    cleanup

--- _book/_book/_book/_book/hyvor.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-    var HYVOR_TALK_WEBSITE = 6232;
-    var HYVOR_TALK_CONFIG = {
-        url: false,
-        id: false
-    };
-</script>
-<script async type="text/javascript" src="//talk.hyvor.com/web-api/embed.js"></script>
